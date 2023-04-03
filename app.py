4e207b0a701c8f3ecb024de7505593d11864aed7
Mahammadjan <m.mehemmedcanov@gmail.com>
2023-04-03 20:26:54 +0400

    edited

--- app.py
+++ app.py
@@ -62,6 +62,3 @@ async def predict(item: email_spam_detection):
     return {"prediction": result,
             "is_spam": is_spam,
             "confidence": confidence}
-
-if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, log_level="info")
